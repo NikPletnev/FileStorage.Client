c74e1067158811317e4b12216634ffd1c9b1e5b3
Nikita <klophah@gmail.com>
2022-05-29 14:29:44 +0300

    Add project files.

---
+++ FileStorageClient.UI/Commands/MainWindowCommands/LoginCommand.cs
@@ -0,0 +1,23 @@
+﻿using FileStorageClient.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStorageClient.UI.Commands.MainWindowCommands
+{
+    internal class LoginCommand : CommandBase
+    {
+        private MainWindowViewModel _viewModel;
+        public LoginCommand(MainWindowViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+
+        }
+    }
+}
---
+++ FileStorageClient.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using FileStorageClient.UI.Commands.MainWindowCommands;
+using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FileStorageClient.UI.ViewModels
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        public ICommand Login { get; set; }
+
+        private string _nameTextBox;
+        private string _passwordTextBox;
+        public string NameTextBox
+        {
+            get { return _nameTextBox; }
+            set
+            {
+                _nameTextBox = value;
+                OnPropertyChanged(nameof(NameTextBox));
+            }
+        }
+
+        public string PasswordTextBox
+        {
+            get { return _passwordTextBox; }
+            set 
+            {
+                _passwordTextBox = value;
+                OnPropertyChanged(nameof(PasswordTextBox));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Login = new LoginCommand(this);
+        }
+
+    }
+}
